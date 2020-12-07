22b5507d952c1f395a9ef75b65e8d018742b6f1c
Kosmikas <short.spin@gmail.com>
2020-12-07 21:09:30 +0200

    corrected class

---
+++ MedziagosKartojimas/BaseClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+    public abstract class BaseClass
+
+    {
+            public string Name { get; }
+            public BaseClass(string name)
+            {
+                Name = name;
+            }
+
+            public abstract string NameMetodas();
+
+            public abstract int NameKodas(int a);
+
+
+
+
+
+        }
+    }
+
--- MedziagosKartojimas/Program.cs
+++ MedziagosKartojimas/Program.cs
@@ -7,26 +7,8 @@ namespace MedziagosKartojimas
         static void Main(string[] args)
         {
             
-
         }
     }
-    public abstract class BaseClass
-    {
-        public string Name { get; }
-        public BaseClass(string name)
-        {
-            Name = name;
-        }
-
-        public abstract string NameMetodas();
-
-        public abstract int NameKodas(int a);
-        
-      
     
-
-        
-    }
-
 
 }
