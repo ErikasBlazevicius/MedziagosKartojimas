0825250bcc6e034a17928f5db0fffecb3e3d17e9
Kosmikas <short.spin@gmail.com>
2020-12-07 20:57:45 +0200

    abstract

--- MedziagosKartojimas/Program.cs
+++ MedziagosKartojimas/Program.cs
@@ -12,10 +12,21 @@ namespace MedziagosKartojimas
     }
     public abstract class BaseClass
     {
-         public string Name { get; }
-        public BaseClass()
+        public string Name { get; }
+        public BaseClass(string name)
         {
-                
+            Name = name;
         }
+
+        public abstract string NameMetodas();
+
+        public abstract int NameKodas(int a);
+        
+      
+    
+
+        
     }
+
+
 }
