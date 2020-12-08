fde7ca770b7a447cb36ebb5b5ac966b5ea86f76f
Kosmikas <short.spin@gmail.com>
2020-12-08 19:38:27 +0200

    char to ascii

--- MedziagosKartojimas/AKlase.cs
+++ MedziagosKartojimas/AKlase.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+
 
 namespace MedziagosKartojimas
 {
-    public class AKlase
+    public class AKlase : BaseClass
     {
+       
+        public AKlase(string name) : base(name)
+        {
 
+        }
+        public override string NameMetodas()
+        {
+            var sb = new StringBuilder();
+            List<char>balses = new List<char>{ 'a', 'e', 'i', 'y', 'o', 'u'};
+            
+            foreach (var raide in Name)
+            {
+                sb.Append(balses.Contains(raide) ? (int)raide : raide);
+            }
+
+            return sb.ToString();
+
+        }
+
+
+        public override int NameKodas(int a)
+        {
+            
+        }
     }
 }
---
+++ MedziagosKartojimas/Interface/IZodisKitaip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas.Interface
+{
+    public interface IZodisKitaip
+    {
+        public string ZodisBeBalsiu();
+        public string ZodisBePriebalsiu();
+        public string ZodisSuPakeistomisBalsemis();
+        public string ZodisSuPakeistomisPriebalsemis();
+
+    }
+}
